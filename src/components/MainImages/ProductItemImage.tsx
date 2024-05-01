d02a17abfd458726f22d3e1b2e47b985fc35a600
fatmakhan24 <khanfatma78987@gmail.com>
2024-05-01 17:58:43 +0530

    #-86cv2ah0n-product-get

--- src/components/MainImages/ProductItemImage.tsx
+++ src/components/MainImages/ProductItemImage.tsx
@@ -1,59 +1,75 @@
 import baseURL from '../../utils/axios';
 import DefaultLayout from '../../layout/DefaultLayout';
-import { Outlet, useNavigate } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState, useRef } from 'react';
 import Breadcrumb from '../Breadcrumbs/Breadcrumb';
 import axios from 'axios';
-import TableLoader from '../../components/Loaders/TableLoader'
 
 const ProductItemImage = () => {
   const [image, setImage] = useState('');
   const [Id, setId] = useState(null);
-  const [productItem, setProductItem] = useState([null]);
-  // const [isLoading, setIsLoading] = useState(true); // Track loading state
+  const [productItem, setProductItem] = useState<any>(null);
+  const fileInputRef = useRef(null);
 
-  const navigate = useNavigate();
-
-  const handleImage = (event) => {
-    setImage(event.target.files[0]);
-    console.log(event.target.file[0]);
-  };
-
-  const handleClick = () => {
-    const formDataa = new FormData();
-    formDataa.append('image', image);
-
-    axios
-      .post(baseURL + 'saveImage/', formDataa, {
-        headers: {
-          'Content-Type': 'multipart/form-data', // Set the content type to multipart/form-data
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      });
-
-    navigate('../');
-  };
+  useEffect(() => {
+    getProductItem();
+  }, []);
 
   const getProductItem = () => {
     axios
       .get(baseURL + 'productItem/')
       .then((res) => {
-        setProductItem(res.data.object);
+        setProductItem(res.data.items);
+        console.log(res)
       })
       .catch((err) => {
         console.log(err);
-      })
-
-      // .finally(() => {
-      //   setIsLoading(false); // Turn off loading indicator regardless of success or failure
-      // });
+      });
   };
 
-  useEffect(() => {
-    getProductItem();
-  }, []);
+  const handleImage = (event: any) => {
+    const file = event.target.files[0];
+    if (file) {  
+      setImage(file);
+      console.log(file);
+    }
+  };
+
+  const handleClick = () => { 
+
+    // let data = {
+    //   productItem : item_name,
+    // }
+
+    const formData = new FormData();
+    formData.append('mainImage', image);
+    formData.append('productName',JSON.stringify(Id)); 
+
+    axios
+      .post(baseURL + '/saveImage'+ Id, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+ 
+  const deleteHandler = () => { 
+  
+    axios
+      .delete(baseURL + `/mainImage/${Id}`)
+      .then((res) => {
+        console.log('Image deleted successfully:', res.data);
+        
+      })
+      .catch((error) => {
+        console.error('Error deleting image:', error);
+        
+      });
+  };
+
 
   return (
     <DefaultLayout>
@@ -66,28 +82,23 @@ const ProductItemImage = () => {
           Product Item Name
         </label>
 
-        {productItem !==null ? (
-             
-               "loading...."
-              ) : (
+        {!productItem ? (
+          'loading....'
+        ) : (
+          <select
+            onChange={(event: any) => setId(event.target.value)}
+            className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+          >
+            <option value={Id}>Select a Product Items </option>
 
-        <select
-          onChange={(event) => setId(event.target.value)}
-          className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-          
-        >
-          <option value={Id}>Select a Product Items </option>
-
-          {productItem.map((Item) => (
-            <option
-              key={Item.item_id}
-              value={Item.item_id}
-            >
-              {Item.items?.item_name}
-            </option>
-          ))}
-        </select>
-          )}
+            {productItem?.map((item: any) => (
+              <option key={item.item_id} value={item.item_id}>
+                {item.item_name}
+               
+              </option>
+            ))}
+          </select>
+        )}
       </div>
 
       <div>
@@ -95,16 +106,25 @@ const ProductItemImage = () => {
           Product Item Image
         </label>
         <input
+          ref={fileInputRef}
           type="file"
-          onChange={handleImage}
+          onChange={handleImage} 
           className="w-full border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
         />
         <button
           onClick={handleClick}
           type="submit"
-          className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90"
+          className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-2"
         >
           Submit
+        </button>
+
+        <button
+          onClick={deleteHandler}
+          type="submit"
+          className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-2"
+        >
+          Delete
         </button>
       </div>
     </DefaultLayout>
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 
-const baseURL= "https://de91-2405-201-3037-e833-491a-f0f8-e381-b453.ngrok-free.app/"
+const baseURL= "https://f0d3-2405-201-3037-e07c-d42-bf10-c5ea-7a27.ngrok-free.app/"
+// const baseURL= "https://q10wk67zmo0m.share.zrok.io/"
 
 export default baseURL;
