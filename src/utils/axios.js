6207265bb0822218b064296e545443f4751ce6c6
fatmakhan24 <khanfatma78987@gmail.com>
2024-05-03 14:40:33 +0530

    #-86cv2ah0n-productItem

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 
-const baseURL= "https://d3f4-2405-201-3037-e07c-25a1-b2c0-eb8c-1ced.ngrok-free.app/"
+const baseURL= "https://ea7f-2405-201-3037-e07c-2862-d0ae-64ea-7eee.ngrok-free.app/"
 // const baseURL= "https://q10wk67zmo0m.share.zrok.io/"
 
 export default baseURL; 
