7978e7ca79d9277015921543c51a9f25517bb5f8
fatmakhan24 <khanfatma78987@gmail.com>
2024-05-07 18:34:20 +0530

    #-86cv2ah0n-void

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 
-const baseURL= "https://ea7f-2405-201-3037-e07c-2862-d0ae-64ea-7eee.ngrok-free.app/"
+const baseURL= "https://7eb7-2405-201-3044-480b-ed2f-12bc-fdf2-b52b.ngrok-free.appp/"
 // const baseURL= "https://q10wk67zmo0m.share.zrok.io/"
 
 export default baseURL; 
