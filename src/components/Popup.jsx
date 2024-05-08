0975e06b29cc6fdd5ce5025ca23d93f46b4eea60
shumailashafiq952@gmail.com <bushra2.infomover@gmail.com>
2024-05-08 12:36:20 +0530

    darkStore-by-vendor

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import baseURL from '../utils/axios';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 
 function Popup({ showPopup,togglePopup,selectedId}) { 
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,7 @@
 // })
 
 // const baseURL= "https://e18j50xdfzc9.share.zrok.io/"
-const baseURL= "https://7eb7-2405-201-3044-480b-ed2f-12bc-fdf2-b52b.ngrok-free.app/"
+const baseURL= "https://6757-2405-201-3044-480b-3c39-e00a-4690-db41.ngrok-free.app/"
 
 
 
