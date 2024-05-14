870774e2a35e19d299b1a3e7d33aa1ca24f3eb17
fatmakhan24 <khanfatma78987@gmail.com>
2024-05-14 13:42:09 +0530

    #-86cv2ah0n-complete

--- src/components/MainImages/ProductItemImage.tsx
+++ src/components/MainImages/ProductItemImage.tsx
@@ -14,6 +14,7 @@ const ProductItemImage = () => {
   const [showImageInput, setShowImageInput] = useState(false);
   const [showOptionDropdown, setShowOptionDropdown] = useState(false);
   const [selectedOption, setSelectedOption] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
 
   useEffect(() => {
     getProductItem();
@@ -44,7 +45,7 @@ const ProductItemImage = () => {
     }
   };
 
-  const handleSelectChange = (event:any) => {
+  const handleSelectChange = (event: any) => {
     const selectedItemId = event.target.value;
     const selectedProductItem = productItem.find(
       (item) => item.item_id === parseInt(selectedItemId),
@@ -55,17 +56,16 @@ const ProductItemImage = () => {
     setSelectedVariationOptions(
       selectedProductItem.variations.flatMap((variation) => variation.options),
     );
-    // setId(selectedItemId);
+    setId(selectedItemId);
     setShowImageInput(true);
     setShowOptionDropdown(true);
+    setShowDelete(true);
   };
 
-  const handleOptionSelectChange = (event:any) => {
+  const handleOptionSelectChange = (event: any) => {
     setSelectedOption(event.target.value);
-    setShowImageInput(true); 
+    setShowImageInput(true);
   };
-
-  
 
   //  const handleSubmit = (event:any) => {
   //   event.preventDefault();
@@ -86,57 +86,22 @@ const ProductItemImage = () => {
   //     });
   // };
 
-  const handleSubmit = (event:any) => {
-     event.preventDefault();
-
-    const formData = new FormData();
-    // formData.append('VariationImage', image);
-    // formData.append('VariationImageData', JSON.stringify(Id));
-    formData.append('ProductItemId', Id);
-    selectedVariationOptions.forEach((option) => {
-      formData.append('VariationOptionIds', option.VOID);
-    });
-    image.forEach((image) => {
-      formData.append(`VariationImage`, image);
-    });
-
-    axios
-      .post(baseURL + `upload`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      });
-  };
-
-  // const handleSubmit = (event:any) => {
-    // event.preventDefault();
+  // const handleSubmit = (event: any) => {
+  //   event.preventDefault();
 
   //   const formData = new FormData();
-
+  //   // formData.append('VariationImage', image);
+  //   // formData.append('VariationImageData', JSON.stringify(Id));
+  //   formData.append('ProductItemId', Id);
+  //   selectedVariationOptions.forEach((option) => {
+  //     formData.append('VariationOptionIds', selectedOption);
+  //   });
   //   image.forEach((image) => {
   //     formData.append(`VariationImage`, image);
   //   });
 
-  //   formData.append('productItemId', Id);
-  //   // formData.append("variationOptionId", JSON.stringify(Id));
-
-  //   formData.append(
-  //     'VariationImageData',
-  //     JSON.stringify({
-  //       ProductItemId: Id,
-  //       // variationOptionId: Id,
-  //     }),
-  //   );
-
-  //   selectedVariationOptions.forEach((option) => {
-  //     formData.append('variationOptionId', option.VOID);
-  //   });
-
   //   axios
-  //     .post(baseURL + `upload/`, formData, {
+  //     .post(baseURL + `upload`, formData, {
   //       headers: {
   //         'Content-Type': 'multipart/form-data',
   //       },
@@ -145,6 +110,66 @@ const ProductItemImage = () => {
   //       console.log(res.data);
   //     });
   // };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('ProductItemId', Id);
+    selectedVariationOptions.forEach((option) => {
+      formData.append('VariationOptionIds', selectedOption);
+    });
+    image.forEach((image) => {
+      formData.append(`VariationImage`, image);
+    });
+
+    // Check if selectedOption is not null, indicating that an option has been selected
+    
+    if (selectedOption) {
+      // Call the 'upload' API
+      axios
+        .post(baseURL + `upload`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          
+        })
+        .catch((error) => {
+          console.error('Error uploading image:', error);
+        
+        });
+    } else {
+
+      // Call the 'saveImage' API
+
+      const formData = new FormData();
+      image.forEach((image) => {
+        formData.append(`MainImage`, image);
+      });
+      formData.append(
+        'MainImageData',
+        JSON.stringify({ productItemId: parseInt(Id) }),
+      );
+
+      axios
+        .post(baseURL + `saveImage`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          
+        })
+        .catch((error) => {
+          console.error('Error saving image:', error);
+          
+        });
+    }
+  };
 
   const deleteHandler = (Id: number) => {
     axios
@@ -166,124 +191,137 @@ const ProductItemImage = () => {
       <div>
         <div>
           <form onSubmit={handleSubmit}>
-          <div>
-            <div className="mb-4.5">
-              <label className="mb-2.5 block text-black dark:text-white">
-                Product Item Name
-              </label>
+            <div>
+              <div className="mb-4.5">
+                <label className="mb-2.5 block text-black dark:text-white">
+                  Product Item Name
+                </label>
 
-              {!productItem ? (
-                'loading....'
-              ) : (
-                <select
-                  onChange={handleSelectChange}
-                  className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                >
-                  <option value={Id}>Select a Product Items </option>
+                {!productItem ? (
+                  'loading....'
+                ) : (
+                  <select
+                    onChange={handleSelectChange}
+                    className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                  >
+                    <option value={Id}>Select a Product Items </option>
 
-                  {productItem?.map((item: any) => (
-                    <option key={item.item_id} value={item.item_id}>
-                      {item.item_name}
-                    </option>
+                    {productItem?.map((item: any) => (
+                      <option key={item.item_id} value={item.item_id}>
+                        {item.item_name}
+                      </option>
+                    ))}
+                  </select>
+                )}
+              </div>
+
+              <div>
+                <div>
+                  {image.map((image, index) => (
+                    <div
+                      key={index}
+                      className="h-[50px] w-[70px] relative overflow-hidden rounded"
+                    >
+                      <img
+                        className="h-[50px] w-[70px] object-cover"
+                        src={baseURL + image.url}
+                        alt=""
+                      />
+                    </div>
                   ))}
-                </select>
+                  {/* <div>
+                    {showDelete && (
+                      <button
+                        onClick={() => deleteHandler(Id)}
+                        className=" shadow bg-primary py-2 px-4 font-medium text-gray hover:bg-opacity-90 mt-2"
+                      >
+                        X
+                      </button>
+                    )}
+                  </div> */}
+                </div>
+              </div>
+
+              {showImageInput && (
+                <div>
+                  <label className="mb-2.5 mt-5 block text-black dark:text-white">
+                    Product Main Image
+                  </label>
+                  <input
+                    ref={fileInputRef}
+                    type="file"
+                    onChange={handleImage}
+                    multiple
+                    className="w-full border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                  />
+                </div>
               )}
             </div>
 
             <div>
-              <div>
-                
+              <div className="mt-5 mb-10">
+                {showOptionDropdown && (
+                  <div>
+                    <label className="mb-2.5 block text-black dark:text-white">
+                      Option Item Name
+                    </label>
 
-                {image.map((image, index) => (
-                  <div
-                    key={index}
-                    className="h-[50px] w-[70px] relative overflow-hidden rounded"
-                  >
-                    <img
-                      className="h-[50px] w-[70px] object-cover"
-                      src={baseURL + image.url}
-                      alt=""
+                    {!productItem ? (
+                      'loading....'
+                    ) : (
+                      <select
+                        // onChange={(event) => handleSelectChange(event)}
+                        onChange={handleOptionSelectChange}
+                        className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                      >
+                        <option value={Id}>Select an option Items </option>
+
+                        {selectedVariationOptions.map((option) => (
+                          <option key={option.VOID} value={option.VOID}>
+                            {option.value}
+                          </option>
+                        ))}
+                      </select>
+                    )}
+                  </div>
+                )}
+                {selectedOption && (
+                  <div>
+                    <label className="mb-2.5 block text-black dark:text-white mt-5">
+                      Option Image
+                    </label>
+                    <input
+                      ref={fileInputRef}
+                      type="file"
+                      onChange={handleImage}
+                      multiple
+                      className="w-full border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                     />
                   </div>
-                ))}
+                )}
               </div>
             </div>
 
-            {showImageInput && (
-              <div>
-                <label className="mb-2.5 block text-black dark:text-white">
-                  Main Image
-                </label>
-                <input
-                  ref={fileInputRef}
-                  type="file"
-                  onChange={handleImage}
-                  multiple
-                  className="w-full border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-2"
+            >
+              Submit
+            </button>
+          </form>
+
+          <div className="mt-5">
+            <h1 className='mb-2.5 block text-black dark:text-white">Delete Images'></h1>
+
+            {showDelete && (
+              <button
+                onClick={() => deleteHandler(Id)}
+                className=" shadow bg-primary py-2 px-4 font-medium text-gray hover:bg-opacity-90 mt-2"
+              >
+                Delete
+              </button>
             )}
           </div>
-
-          <div>
-            <div className="mt-5 mb-10">
-              {showOptionDropdown && (
-                <div>
-                  <label className="mb-2.5 block text-black dark:text-white">
-                    Option Item Name
-                  </label>
-
-                  {!productItem ? (
-                    'loading....'
-                  ) : (
-                    <select
-                      // onChange={(event) => handleSelectChange(event)}
-                      onChange={handleOptionSelectChange}
-                      className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                    >
-                      <option value={Id}>Select an option Items </option>
-
-                      {selectedVariationOptions.map((option) => (
-                        <option key={option.VOID} value={option.VOID}>
-                          {option.value}
-                        </option>
-                      ))}
-                    </select>
-                  )}
-                </div>
-              )}
-               {selectedOption && (
-              <div>
-                <label className="mb-2.5 block text-black dark:text-white mt-5">
-                  Option Image
-                </label>
-                <input
-                  ref={fileInputRef}
-                  type="file"
-                  onChange={handleImage}
-                  multiple
-                  className="w-full border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                />
-              </div>
-                )}
-            </div>
-          </div>
-
-          <button
-            type="submit"
-            className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-2"
-          >
-            Submit
-          </button>
-
-          <button
-            onClick={() => deleteHandler(Id)}
-            type="submit"
-            className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray hover:bg-opacity-90 mt-2"
-          >
-            Delete
-          </button>
-          </form>
         </div>
       </div>
     </DefaultLayout>
