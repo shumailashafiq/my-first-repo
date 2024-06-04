76ad6b3b28763ae8ae3c17670a1ed829af5be4a8
shumailashafiq952@gmail.com <bushra2.infomover@gmail.com>
2024-06-04 15:27:56 +0530

    #feature-86cvgqn68-show-all-items

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ import Products from './pages/Products';
 // import AddProduct from './components/Products/AddProduct'
 
 import Orders from './pages/Orders';
+import Items from './pages/Items';
 
 import { ProtectedRoutes } from './components/ProtectedRoutes';
 import { IsAuthRoutes } from './components/IsAuthRoutes';
@@ -151,7 +152,18 @@ function App() {
             <Route path="add" element={<AddProduct />} />
           </Route>
 
-          
+          <Route
+            path="/items/"
+            element={
+              <>
+                <PageTitle title="Items | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                <Items />
+              </>
+            }
+          >
+            
+          </Route>
+
 
           {/* <Route
             path="/categories/main-category"
