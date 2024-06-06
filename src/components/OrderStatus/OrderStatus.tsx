dc80d44dd221a2c226fb3dadc5f3ebc288697704
fatmakhan24 <khanfatma78987@gmail.com>
2024-06-07 00:10:05 +0530

    #86cvfxcdm-orderStatus-Completed

--- src/components/OrderStatus/OrderStatus.tsx
+++ src/components/OrderStatus/OrderStatus.tsx
@@ -5,33 +5,44 @@ import baseURL from '../../utils/axios';
 const OrderStatus = ({ OrderIndex, setactive }) => {
   const [orderStatusData, setOrderStatusData] = useState([]);
   const [completedSteps, setCompletedSteps] = useState(0);
-
-  // useEffect(() => {
-  //   if (completedSteps < orderStatusData.length && orderStatusData[completedSteps]?.OrderStatus !== 'cancelled') {
-  //     const timer = setTimeout(() => {
-  //       setCompletedSteps((prev) => prev + 1);
-  //     }, 2000);
-  //     return () => clearTimeout(timer);
-  //   }
-  // }, [completedSteps, orderStatusData]);
-
-  useEffect(() => {
-    if (
-      completedSteps < orderStatusData.length &&
-      orderStatusData[completedSteps]?.OrderStatus !== 'cancelled'
-    ) {
-      const timer = setTimeout(() => {
-        setCompletedSteps((prev) => prev + 1);
-      }, 2000);
-      return () => clearTimeout(timer);
-    }
-  }, [completedSteps, orderStatusData]);
+  const [isCancelled, setIsCancelled] = useState(false);
 
   useEffect(() => {
     if (OrderIndex) {
       getOrderStatusData();
     }
   }, [OrderIndex]);
+
+  useEffect(() => {
+    if (!isCancelled && completedSteps < orderStatusData.length) {
+      const currentStatus = orderStatusData[completedSteps]?.OrderStatus;
+
+      if (currentStatus === 'cancelled') {
+        const timer = setTimeout(() => {
+          setIsCancelled(true);
+          setOrderStatusData((prevData) => {
+            return prevData.map((order, index) => {
+              if (index === completedSteps) {
+                return {
+                  ...order,
+                  currentStep: 'cancelled',
+                  previousStep: index - 1,
+                };
+              }
+              return order;
+            });
+          });
+        }, 2000);
+
+        return () => clearTimeout(timer);
+      } else {
+        const timer = setTimeout(() => {
+          setCompletedSteps((prev) => prev + 1);
+        }, 2000);
+        return () => clearTimeout(timer);
+      }
+    }
+  }, [completedSteps, orderStatusData, isCancelled]);
 
   const getOrderStatusData = () => {
     axios
@@ -62,12 +73,13 @@ const OrderStatus = ({ OrderIndex, setactive }) => {
           'loading....'
         ) : (
           <div>
-            <h2 className="text-[25px] text-black text-bold">Order Status</h2>
             <div className="mt-4">
               {orderStatusData.length > 0 && (
-                <div className="flex justify-between space-x-5">
+                <div className="flex  space-x-5">
                   <h1 className="text-[26px] text-black">Order Id :</h1>
-                  <td>{orderStatusData[0]?.order_id}</td>
+                  <td className="text-[19px] text-black">
+                    {orderStatusData[0]?.order_id}
+                  </td>
                 </div>
               )}
             </div>
@@ -75,168 +87,64 @@ const OrderStatus = ({ OrderIndex, setactive }) => {
         )}
       </div>
 
-      {/* <div className="status-container mt-10 mb-20 flex items-center space-x-10 relative">
+      <div className="status-container mt-10 mb-20 flex items-center space-x-10 relative">
         {orderStatusData.map((order, key) => (
           <div className="flex flex-col items-center relative" key={key}>
             <div className="flex items-center justify-center w-20 h-20 bg-white border-2 border-gray-500 rounded-full">
-              {key < completedSteps ? (
-                order.OrderStatus === 'cancelled' ? (
-                  <svg
-                    className="w-10 h-10 text-red-900"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="w-10 h-10 text-green-500"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                )
+              {order.currentStep === 'cancelled' ||
+              (isCancelled && key === completedSteps + 1) ? (
+                <svg
+                  className="w-10 h-10 text-red-900"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : key < completedSteps ? (
+                <svg
+                  className="w-10 h-10 text-green-500"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
               ) : (
                 <p className="text-gray-500">{order?.OrderStatus}</p>
               )}
             </div>
             <p className="mt-2 text-gray-500">{order?.OrderStatus}</p>
-            {key < orderStatusData.length - 1 && (
-              <div
-                className={`absolute top-1/3 left-full transition-duration: 500 w-10 h-1 ${
-                  key < completedSteps - 1 ? 'bg-green-500' : 'bg-gray-500'
-                }`}
-              ></div>
-            )}
-          </div>
-        ))}
-      </div> */}
 
-{/* <div className="status-container mt-10 mb-20 flex items-center space-x-10 relative">
-  {orderStatusData.map((order, key) => {
-    const isPreviousStatusCancelled =
-      key > 0 && orderStatusData[key - 1].OrderStatus === 'cancelled';
-    const isCurrentStatusCancelled = order.OrderStatus === 'cancelled';
 
-    return (
-      <div className="flex flex-col items-center relative" key={key}>
-        <div className="flex items-center justify-center w-20 h-20 bg-white border-2 border-gray-500 rounded-full">
-          {isPreviousStatusCancelled || isCurrentStatusCancelled ? (
-            <svg
-              className="w-10 h-10 text-red-900"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : key < completedSteps ? (
-            <svg
-              className="w-10 h-10 text-green-500"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 13l4 4L19 7"
-              />
-            </svg>
-          ) : (
-            <p className="text-gray-500">{order?.OrderStatus}</p>
-          )}
-        </div>
-        <p className="mt-2 text-gray-500">{order?.OrderStatus}</p>
-        {!isPreviousStatusCancelled &&
-          !isCurrentStatusCancelled &&
-          key < orderStatusData.length - 1 && (
-            <div
-              className={`absolute top-1/3 left-full transition-duration: 500 w-10 h-1 ${
-                key < completedSteps - 1 ? 'bg-green-500' : 'bg-gray-500'
-              }`}
-            ></div>
-          )}
-      </div>
-    );
-  })}
-</div> */}
-
-      <div className="status-container mt-10 mb-20 flex items-center space-x-10 relative">
-        {orderStatusData.map((order, key) => {
-          const isCancelled = order.OrderStatus === 'cancelled';
-
-          return (
-            <div className="flex flex-col items-center relative" key={key}>
-              <div className="flex items-center justify-center w-20 h-20 bg-white border-2 border-gray-500 rounded-full">
-                {isCancelled ? (
-                  <svg
-                    className="w-10 h-10 text-red-900"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                ) : key < completedSteps ? (
-                  <svg
-                    className="w-10 h-10 text-green-500"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                ) : (
-                  <p className="text-gray-500">{order?.OrderStatus}</p>
-                )}
-              </div>
-              <p className="mt-2 text-gray-500">{order?.OrderStatus}</p>
-              {!isCancelled && key < orderStatusData.length - 1 && (
+            {order.currentStep !== 'cancelled' &&
+              key < orderStatusData.length - 1 && (
                 <div
                   className={`absolute top-1/3 left-full transition-duration: 500 w-10 h-1 ${
                     key < completedSteps - 1 ? 'bg-green-500' : 'bg-gray-500'
                   }`}
                 ></div>
               )}
-            </div>
-          );
-        })}
+            {order.currentStep === 'cancelled' && key > 0 &&(
+              <div className="absolute top-1/3 left-full transition-duration: 500 w-10 h-1 bg-red-500"></div>
+            )}
+            {order.currentStep === 'cancelled' && key > 0 && (
+              <div className="absolute top-1/3 left-full transition-duration: 500 w-10 h-1 bg-red-500"></div>
+            )}
+          </div>
+        ))}
       </div>
 
       <div className="table-wrapper">
